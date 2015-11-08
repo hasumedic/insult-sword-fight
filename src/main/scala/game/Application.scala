e67e47e83fefdc8745e41914f68c9c9fff9aa2c7
Alex Salguero <hasumedic@gmail.com>
2015-11-08 23:57:27 +0000

    Added basic structure to the game

--- src/main/scala/game/Application.scala
+++ src/main/scala/game/Application.scala
@@ -3,5 +3,13 @@ package game
 object Application extends App {
 
   val game = new GameEngine
+  game.welcome()
+
+  while (!game.isOver) {
+    game.nextFight()
+  }
+
+  if (game.isWinner) game.goodbyePirate()
+  else game.goodbyeLooser()
 }
 
--- src/main/scala/game/GameEngine.scala
+++ src/main/scala/game/GameEngine.scala
@@ -1,7 +1,48 @@
 package game
 
 import model._
+import scala.io.Source
 
 class GameEngine {
-  val repository = new InsultRepository
+  val repository = {
+    val json = Source.fromURL(getClass.getResource("/monkey-island.json")).mkString
+    new InsultRepository(json)
+  }
+
+  private val fightEngine = new FightEngine(repository)
+  private var over = false
+  private var winner = false
+  private var player = Player.buildFrom(repository.takeRandom(2))
+
+  private def finishGame(): Unit = over = true
+
+  def isOver: Boolean = over
+
+  def isWinner: Boolean = winner
+
+  def nextFight(): Unit = {
+    if (fightEngine.playerWantsToFight()) {
+      player = fightEngine.startFight(player)
+      finishGame()
+    } else finishGame()
+  }
+
+  def welcome(): Unit = {
+    println(Console.BLUE + "Welcome to the Monkey Island insult sword-fighting game!" + Console.RESET)
+    println()
+    println("So I've heard that you wanted to be a pirate, huh? Shouldn't you stop crawling and learn how to walk first? HA! Just kidding...")
+    println("To become a real pirate, you first need to defeat the Sword Master™ in single combat. " +
+      "Woah! But stop moving your sword like a feather duster! Combats here are not the way you might think... " +
+      "Here we fight with sharp tongues! And brains!")
+    println("You'll need to learn proper insults from other pirates before you are ready to face the Sword Master™. " +
+      "The more insults you learn, the sooner you'll be ready to face the Sword Master™! Oh look, here comes a fellow pirate!")
+  }
+
+  def goodbyePirate() = {
+    println("Oh! So you learned how to fight in the end! Well done son, use your sharp tongue wisely now!")
+  }
+
+  def goodbyeLooser() = {
+    println("Coward...")
+  }
 }
